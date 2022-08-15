ab8921a901a45f7b26f603f52e73f4a55eef1230
Kathurjan <kath097c@easv365.dk>
2022-08-15 14:48:27 +0200

    Update Service.cs

--- Service.cs
+++ Service.cs
@@ -4,22 +4,31 @@ public class Service: IService
 {
     public object FindElement(IEnumerable<object> data, int index)
     {
-        throw new NotImplementedException();
+        return new List<object>(data).IndexOf(index);
     }
 
     public bool AreTheseStringsEqual(string str1, string str2)
     {
-        throw new NotImplementedException();
+        return str1 == str2;
     }
 
     public int Factorial(int n)
     {
-        throw new NotImplementedException();
+        if(n==1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n*(Factorial(n-1));
+        }
     }
 
     public int Fibonacci(int n)
     {
-        throw new NotImplementedException();
+        if (n == 0) return 0; //To return the first Fibonacci number   
+        if (n == 1) return 1; //To return the second Fibonacci number   
+        return Fibonacci(n - 1) + Fibonacci(n - 2);  
     }
 
     public IEnumerable<int> FindValuesGreaterThan(IEnumerable<int> data, int value)
@@ -29,22 +38,68 @@ public class Service: IService
 
     public double FindAverage(IEnumerable<int> data)
     {
-        throw new NotImplementedException();
+        return data.Average();
     }
 
     public string SwapCharacters(string testString, char swapA, char swapB)
     {
-        throw new NotImplementedException();
+        var newString = "";
+
+        foreach (char character in testString)
+        {
+            if (character.Equals(swapA))
+            {
+                newString = newString + swapB;
+            }
+            else if (character.Equals(swapB))
+            {
+                newString = newString + swapA;
+            }
+            else
+            {
+                newString = newString + character;
+            }
+            
+        }
+
+        return newString;
+
     }
 
     public int ConvertStringToIntIfValidIntegerValue(string testString)
     {
-        throw new NotImplementedException();
+        var returnInt = 0;
+        if (testString.Length > 0 )
+        {
+            for (int i = 0; i <testString.Length ; i++)
+            {
+                returnInt++;
+            }
+        }
+
+        return returnInt;
+    }
+    
+    class InnerClassStudent
+    {
+        public int field;
+        public int Property
+        {
+            get
+            {
+                return field * 2;
+            }
+            set
+            {
+                field = value;
+            }
+        }
     }
 
+    
     public object CreateObjectWithPropretyWhichHasAccessors()
     {
-        throw new NotImplementedException();
+        return new InnerClassStudent();
     }
 
     public IEnumerable<object> CreateListWithObjectsContainingIntAndItsSquare(IEnumerable<int> data)
